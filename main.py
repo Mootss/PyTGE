c6f5d9fb5dde46c23d1b4760002c5fd5f70fd962
unknown <moothymycom@gmail.com>
2025-08-18 17:46:29 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,145 @@
+# 18/8/25
+# to do:
+# implement transparent pixels (impotrant)
+# add option to modify screen border behaviour, aka when sprite hit end of array
+# add option load a background image into bg array
+# handle odd arays for bg array
+# fix displaying text
+# add sprite collision detection
+# make minecraft?
+
+#from random import choice
+import time
+import sys
+import os
+
+def createShape():
+    pass
+
+class Screen():
+    uhb = "\u2580" # upper half block ▀
+    #bit3 = "\u001b"
+    #bit8 = "\033[38;5;0m"
+
+    def __init__(self, colorMode, height, width, color=0):
+        self.colorMode = colorMode
+        # colorMode should be 8 or 256. thats the number of colors to be used
+        # fyi, all 8 color arrays can be displaed in 256 but not vice versa
+        self.height = height
+        self.width = width
+        self.color = color # if undefined, default = black
+        self.array = self.createArray()
+        self.displayArray = [row[:] for row in self.array]
+        # sprite eh draw kuran v ma do it by modifying display array instead of og array, so og array can be re-used when a sprite is moved
+        # im sure theres a more efficient way to do ts but my sikundi too smol
+
+    def createArray(self): # 2d array
+        return [[self.color for x in range(self.width)] for y in range(self.height)]
+
+    def draw(self, fg, bg): # draws 1 character (which is 2 px)
+        if self.colorMode == 8:
+            return f"\u001b[3{fg}m\u001b[4{bg}m{self.uhb}\u001b[0m" # \u001b[0m actually only needs to be written at the end of each line but wtver
+        elif self.colorMode == 256:
+            return f"\033[38;5;{fg}m\033[48;5;{bg}m{self.uhb}\u001b[0m"
+        else:
+            raise ValueError("ERROR: Invalid color mode, colorMode should be either 8 or 256")
+    
+    def render(self): # convert array to ansi & print it
+        # todo: add a way to render transparent pixels aka no color pixels
+        print("\033[2;1H", end="")
+        lines = []
+        pixels = []
+        for y in range(0, len(self.displayArray), 2):
+            for fg, bg in zip(self.displayArray[y], self.displayArray[y+1]):
+                pixels.append(self.draw(fg, bg))
+                #print(draw(fg, bg))
+            lines.append("".join(pixels))
+            pixels = []
+            #print(lines)
+        window = "\n".join(lines)
+        print(window)
+        self.reset()
+
+    def reset(self):
+        #self.displayArray = [[e for e in row] for row in self.array]
+        self.displayArray = [row[:] for row in self.array]
+
+    def drawSprite(self, sprite):
+        # todo: make it so it doenst insert transparent pixels
+        # todo: add a check to see if there is enough space to insert the sprite
+        for row in range(len(sprite.array)):
+            self.displayArray[sprite.posY + row][sprite.posX:(sprite.posX + len(sprite.array[row]))] = sprite.array[row]
+            # basically, take a section of display array and replace it w sprite   
+        
+
+class Sprite():
+    def __init__(self, array, posX, posY):
+        self.array = array
+        self.posX = posX
+        self.posY = posY
+
+    def move(self, direction, i): # i = increment amount
+        direction = direction.lower()
+
+        if direction == "up":
+            self.posY -= i
+        
+        elif direction == "down":
+            self.posY += i
+
+        elif direction == "right":
+            self.posX += i
+
+        elif direction == "left":
+            self.posX -= i
+        else:
+            raise ValueError("ERROR: Invalid direction, accepted directions are: 'up', 'down', 'right', 'left'")
+        
+    def flip():
+        pass
+    
+    def rotate():
+        pass
+
+class InputHandler():
+    #not sure how to write ts
+    pass
+
+
+# input handling, stole this from stackoverflow heh
+if os.name == 'nt':  # Windows
+    import msvcrt
+
+    def getKey():
+        if msvcrt.kbhit():  # Only read if a key was pressed
+            first = msvcrt.getch()
+            if first in {b'\x00', b'\xe0'}:  # Special key (arrows, etc.)
+                second = msvcrt.getch()
+                return second.decode('utf-8', errors='ignore')
+            else:
+                return first.decode('utf-8', errors='ignore')
+        return None  # No key pressed
+
+else:  # Unix (Linux, macOS)
+    import tty
+    import termios
+    import select
+
+    def getKey():
+        dr, _, _ = select.select([sys.stdin], [], [], 0)  # 0 = no wait
+        if dr:
+            fd = sys.stdin.fileno()
+            old_settings = termios.tcgetattr(fd)
+            try:
+                tty.setraw(fd)
+                ch1 = sys.stdin.read(1)
+                if ch1 == '\x1b':  # Arrow keys
+                    ch2 = sys.stdin.read(1)
+                    ch3 = sys.stdin.read(1)
+                    if ch2 == '[':
+                        return ch3  # 'A'=up, 'B'=down, 'C'=right, 'D'=left
+                return ch1
+            finally:
+                termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return None  # No key pressed
+ 
---
+++ sample.py
@@ -0,0 +1,75 @@
+from main import *
+
+# set up the screen here
+screen = Screen(
+    colorMode=256,
+    height=32,
+    width=64,
+    color=59
+    # in 8 colorMode to set red u will write color=1, bec 1 is red, bec 41 and 31 is red. in 8 colormode only use the last digit 
+)
+
+pictureOfMarioInArrayFormatBruh = [[255,255,255,203,203,203,203,203,203,255,255,255],[255,255,203,203,203,203,203,203,203,203,203,203],[255,255,94,94,94,223,223,223,0,223,255,255],[255,94,223,94,223,223,223,223,0,223,223,255],[255,94,223,94,94,223,223,223,223,0,223,223],[255,94,94,223,223,223,223,223,0,0,0,0],[255,255,255,223,223,223,223,223,223,223,223,255],[255,255,203,203,31,203,203,203,203,255,255,255],[255,203,203,203,31,203,203,31,203,203,203,255],[203,203,203,203,31,31,31,31,203,203,203,203],[223,223,203,31,11,31,31,11,31,203,223,223],[223,223,223,31,31,31,31,31,31,223,223,223],[223,223,31,31,31,31,31,31,31,31,223,223],[255,255,31,31,31,31,31,31,31,31,255,255],[255,94,94,94,255,255,255,255,94,94,94,255],[94,94,94,94,255,255,255,255,94,94,94,94]]
+
+# create the MARIO!
+mario = Sprite(
+    array= pictureOfMarioInArrayFormatBruh,
+    posX=5,
+    posY=5
+)
+
+def game():
+    print("\033[2J\33[?25l") # clear screen + hide cursor
+
+    running = True
+    prevTime = time.time()
+    FPS = 1
+
+    while running == True:
+
+        # handle input
+        key = getKey() # getKey returns a key u click
+        if key:
+            if key.lower() == "q":
+                running = False
+
+            elif key in ["H", "A"]: # up
+                mario.move("up", 1)
+                print("up")
+
+            elif key in ["P", "B"]: # down
+                mario.move("down", 1)
+                print("down")
+
+            elif key in ["M", "C"]: # right
+                mario.move("right", 1)
+                print("right")
+
+            elif key in ["K", "D"]: # left
+                mario.move("left", 1)
+                print("left")
+
+            # H P M K is for windows, A B C D is for unix
+
+        # debugging stuff
+        #time.sleep(0.05) # control framerate
+        now = time.time()
+        dt = now - prevTime
+        prevTime = now
+        if dt > 0:
+            currentFPS = 1 / dt
+            FPS = 0.9 * FPS + 0.1 * currentFPS  
+
+        print(f"""
+              
+FPS: {FPS:.2f}
+X: {mario.posX} Y: {mario.posY}
+click 'q' to quit
+""")
+        FPS+=1
+
+        # draw the shits
+        screen.drawSprite(mario) # this inserts the sprite into the screen array but doesnt actually show it on terminal
+        screen.render() # this prints the "screen"
+    
+game()
